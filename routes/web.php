56336cc6a20437ad24a5424a123135477d279b3a
Antonio Junior <juniormendonca@gmail.com>
2023-04-17 08:23:58 +0200

    README updated.

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Please check README for instructions.';
 });
 
+// returns a list of all the users in the database.
 Route::get('/users', [UserController::class, 'index']);
+
+// returns a list of users based on the search parameters provided in the query string.
 Route::get('/users/search', [UserController::class, 'search']);
+
+// returns a single user based on the ID provided in the URL.
 Route::get('/users/{id}', [UserController::class, 'show']);
+
+// returns a list of all the previous experiences of the user specified by the ID provided in the URL.
 Route::get('/users/{id}/previous-experiences', [UserController::class, 'previousExperiences']);
 
+// returns a list of all the previous experiences in the database.
 Route::get('/previous-experiences', [PreviousExperienceController::class, 'index']);
+
+// returns a single previous experience based on the ID provided in the URL.
 Route::get('/previous-experiences/{id}', [PreviousExperienceController::class, 'show']);
